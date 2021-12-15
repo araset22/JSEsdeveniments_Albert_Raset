61d86542d5a946cd2e375da6554ef2c3155f51bf
Albert Raset <albert.raset@institutvidreres.cat>
2021-12-15 12:10:17 +0100

    Exercici 4

--- JSEsdevenimentsAlbert_Raset.js
+++ JSEsdevenimentsAlbert_Raset.js
@@ -1,8 +1,12 @@
-//Exercici 3
+//Exercici 4
 document.onkeydown = function(lletra){
     document.getElementById('teclat').innerHTML=lletra.key
     var contador = document.getElementById('lletres')
     contador.innerHTML += lletra.key + ', '
+    var neteja = document.getElementById('netejar')
+    neteja.onclick = function(){
+        contador.innerHTML =''
+    }
 }
 document.onkeyup=function(){
     document.getElementById('teclat').innerHTML =' '
